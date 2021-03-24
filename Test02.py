32d0d6de286fca3cf6e7d3a67b7459f01c8f29e7
Lhaosv <61543316+Lhaosv@users.noreply.github.com>
2021-03-24 21:38:50 +0800

    格式化输出

---
+++ Test02.py
@@ -0,0 +1,10 @@
+# 格式化输出
+age = 18
+print("我的年龄是 %d"%age)
+print("我的姓名是 %s,我的国籍是 %s"%("雷","中国"))
+print("aaa","bbb","ccc")  #默认用空格隔开
+print("www","baidu","com",sep=".")  #用.隔开，超链接
+print("hello",end="")  # end后什么也不是写表示不换行
+print("world",end="\t")   #end后面写\t表示转义字符
+print("python",end="\n")  #end后面写\n表示换行
+print("end")
